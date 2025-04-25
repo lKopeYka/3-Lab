6b7e1c5f97c6b41fc6733493e60545ac5a946bc1
KopeYka <mihailamont@gmail.com>
2025-04-25 10:03:11 +0300

    Sourse.c

--- Sourse.c
+++ Sourse.c
@@ -8,29 +8,29 @@
 
 
 //Ямонт Михаил Денисович
-#include "header.h"
+#include "Header.h"
 
 int main() {
-    int choice;
+    int user_choice;
 
     do {
         printf("\nMain Menu:\n");
-        printf("1. Task 1 (Division by 4 without division)\n");
-        printf("2. Task 2 (Student Management System)\n");
+        printf("1. Task 1 (Bitwise operations)\n");
+        printf("2. Task 2 (Student management system)\n");
         printf("0. Exit\n");
         printf("Select task: ");
 
-        while (scanf_s("%d", &choice, sizeof(choice)) != 1) {
+        while (scanf_s("%d", &user_choice, sizeof(user_choice)) != 1) {
             printf("Error! Please enter a number: ");
             while (getchar() != '\n');
         }
 
-        switch (choice) {
+        switch (user_choice) {
         case 1:
-            task1();
+            divide_by_four_without_division_operator();
             break;
         case 2:
-            task2();
+            student_management_system();
             break;
         case 0:
             printf("Exiting program...\n");
@@ -38,7 +38,7 @@ int main() {
         default:
             printf("Invalid choice! Please try again.\n");
         }
-    } while (choice != 0);
+    } while (user_choice != 0);
 
     return 0;
 }
